56c776246c28f43d2299e93893cadfc2c6b0b080
Valentin Pelloin <valentinpelloin@gmail.com>
2023-03-25 22:41:14 +0100

    ObjectCard component start

---
+++ brain/client/src/components/ObjectCard.vue
@@ -0,0 +1,33 @@
+<template>
+	<ion-card>
+		<img src="" height="150" width="1000" />
+		<ion-card-header>
+			<ion-card-title>{{ name }}</ion-card-title>
+			<ion-card-subtitle>{{ type }}</ion-card-subtitle>
+		</ion-card-header>
+		<ion-card-content>
+			{{ description }}
+		</ion-card-content>
+	</ion-card>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	name: String,
+	type: String,
+	description: String,
+	image_url: String,
+});
+</script>
+
+<style scoped>
+/* iOS places the subtitle above the title */
+ion-card-header.ios {
+	display: flex;
+	flex-flow: column-reverse;
+}
+img {
+	object-fit: cover;
+}
+</style>
+
--- brain/client/src/views/TabObjects.vue
+++ brain/client/src/views/TabObjects.vue
@@ -11,76 +11,22 @@
         <ion-title size="large">Objects</ion-title>
       </ion-toolbar>
     </ion-header>
-	<ion-card>
-		<img alt="Picture of Saturn" src="https://malak:5090/static/images/saturn.jpg" height="150" width="1000" />
-		<ion-card-header>
-			<ion-card-title>Saturn</ion-card-title>
-			<ion-card-subtitle>Planet of the Solar System</ion-card-subtitle>
-		</ion-card-header>
-
-		<ion-card-content>
-			Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine and a half times that of Earth.
-		</ion-card-content>
-	</ion-card>
-	
-	<ion-card>
-		<img alt="Picture of Jupiter" src="https://malak:5090/static/images/jupiter.jpg" height="150" width="1000"/>
-		<ion-card-header>
-			<ion-card-title>Jupiter</ion-card-title>
-			<ion-card-subtitle>Planet of the Solar System</ion-card-subtitle>
-		</ion-card-header>
-
-		<ion-card-content>
-			Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass more than two and a half times that of all the other planets in the Solar System combined, and slightly less than one one-thousandth the mass of the Sun.
-		</ion-card-content>
-	</ion-card>
-
-
-    <div class="ion-padding">
-      <h1>Animal Facts</h1>
-
-      <h2>Rhinoceros</h2>
-      <img alt="rhino standing near grass" src="https://images.unsplash.com/flagged/photo-1556983257-71fddc36bc75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80" />
-      <p>The rhinoceros gets its name from one of its most notable features: its horns. The word rhinoceros come from the Greek words rhino meaning “nose” and ceros meaning “horn.” The number of horns that a rhino has varies on the species. The two African species (the black rhino and the white rhino) and the Sumatran rhino have two horns, while the Javan rhino and one-horned rhino have one horn.</p>
-
-      <h2>Sea Turtle</h2>
-      <img alt="brown sea turtle in water" src="https://images.unsplash.com/photo-1573551089778-46a7abc39d9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80" />
-      <p>Sea turtles are characterized by a large, streamlined shell and non-retractile head and limbs. Unlike other turtles, sea turtles cannot pull their limbs and head inside their shells. Their limbs are flippers that are adapted for swimming, so they are vulnerable while on land.</p>
-
-      <h2>Giraffe</h2>
-      <img alt="giraffe sticking its tongue out" src="https://images.unsplash.com/photo-1577114995803-d8ce0e2b4aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1746&q=80" />
-      <p>Giraffes are known for their long, tall appearance. They have a small hump on their back like a camel and have a spotted pattern similar to that of a leopard. Because of the combination of these features, some people called the giraffe a “camel-leopard.” That’s where the giraffe’s species name “camelopardalis” comes from.</p>
-
-      <h2>Elephant</h2>
-      <img alt="two grey elephants on grass plains during sunset" src="https://images.unsplash.com/photo-1564760055775-d63b17a55c44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1754&q=80" />
-      <p>Elephants are the largest existing land animal, with massive bodies, large ears, and long trunks. Elephants’ long trunks are multifunctional. They are used to pick up objects, trumpet warnings, greet other elephants, or suck up water for drinking or bathing.</p>
-
-      <h2>Dolphin</h2>
-      <img alt="black and white dolphin in water" src="https://images.unsplash.com/photo-1607153333879-c174d265f1d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80" />
-      <p>Dolphins range in color depending on the species, from white, pearl, and pink to darker shades of brown, gray, blue, and black. They have smooth skin, flippers, and a dorsal fin. They have a long, slender snout with about 100 teeth and a streamlined body. They have a single blowhole on top of their head, which has a flap that opens to reveal a pair of nostrils. The dolphin uses these nostrils for breathing when it surfaces.</p>
-    </div>
+	<ObjectCard
+		name="Saturn"
+		type="Planet of the Solar System"
+		description="Saturn is the sixth planet from the Sun and the second-largest in the Solar System, after Jupiter. It is a gas giant with an average radius of about nine and a half times that of Earth."
+		image_url="https://malak:5090/static/images/saturn.jpg"
+	/>
   </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import ObjectCard from '@/components/ObjectCard.vue';
 
 export default defineComponent({
-	components: { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle },
+	components: { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, ObjectCard },
 });
 </script>
-
-<style scoped>
-  /* iOS places the subtitle above the title */
-  ion-card-header.ios {
-    display: flex;
-    flex-flow: column-reverse;
-  }
-img {
-	object-fit: cover;
-}
-</style>
-
